f04f3a1539446448cb15cc4e9127513959c4a5cf
20481A12F4 <20481A12F4@gecgudlavallerumic.in>
2024-09-19 14:47:08 +0530

    second commit

---
+++ ConsoleApp5/GenericDeligates.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+     class GenericDeligates
+    {
+        public delegate double Delegate1(int x, float y, double z);
+        public delegate void Delegate2(int x, float y, double z);
+        public delegate bool Delegate3(string d);
+       // public static double Add(int x, float y, double z)
+        //{
+          //  return x + y + z;
+        //}
+       // public static void AddNums1(int x, float y, double z)
+        //{
+          //  Console.WriteLine(x + y + z);
+       // }
+        public static bool Check(string s)
+        {
+            if (s.Length > 5)
+                return true;
+            else return false;
+                    
+        }
+        static void Main()
+        {
+            //Delegate1 d = Add;
+            Func<int, float, double, double> d = (x,y,z)=>
+            {
+                return x + y + z;
+            };
+            double result = d.Invoke(10, 3.5f, 6.89);
+            Console.WriteLine(result);
+            // Delegate2 d2 = AddNums1;
+            Action<int, float, double> d2 = (x,y,z)=>
+            {
+                Console.WriteLine(x + y + z);
+            };
+            d2.Invoke(10, 3.5f, 6.89);
+            //Delegate3 d3 = Check;
+            Predicate<string> d3 = Check;
+            bool b=d3.Invoke("chandra");
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp5/LambdaDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public delegate string Greetings(string c);
+    class LambdaDemo
+    {
+        static void Main(string[] args)
+        {
+            Greetings g =(name)=>
+            {
+                return "hello " + name + "vgm";
+            };
+           string str= g.Invoke("chandra");
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
